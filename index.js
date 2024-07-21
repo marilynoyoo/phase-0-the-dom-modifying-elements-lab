289e88d803450ca11765d965dd4e4f895498bb8d
marilynakinyioyoo <marilyn.oyoo@student.moringaschool.com>
2024-07-21 21:08:10 +0300

    added lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Marilyn is the champion"
+document.body.appendChild(newHeader)
